bbafcb22623178116467db8418ad58a5bf0a8db5
Vladimir Volek <vladimir.volek@satoshilabs.com>
2021-04-05 16:05:19 +0200

    feat: eslint

--- src/methods/getAccountInfo.ts
+++ src/methods/getAccountInfo.ts
@@ -3,21 +3,21 @@ import { getBalances, getAddresses } from '../utils/address';
 import { BlockFrostAPI } from '@blockfrost/blockfrost-js';
 
 export default async (
-  blockFrostApi: BlockFrostAPI,
-  publicKey: string,
+    blockFrostApi: BlockFrostAPI,
+    publicKey: string,
 ): Promise<Types.ResponseAccountInfo> => {
-  const externalAddresses = await getAddresses(publicKey, blockFrostApi, 0);
-  const internalAddresses = await getAddresses(publicKey, blockFrostApi, 1);
+    const externalAddresses = await getAddresses(publicKey, blockFrostApi, 0);
+    const internalAddresses = await getAddresses(publicKey, blockFrostApi, 1);
 
-  const addresses = [...externalAddresses, ...internalAddresses];
-  const balances = await getBalances(addresses);
+    const addresses = [...externalAddresses, ...internalAddresses];
+    const balances = await getBalances(addresses);
 
-  const lovelaceBalance = balances.find(b => b.unit === 'lovelace');
-  const tokens = balances.filter(b => b.unit !== 'lovelace');
+    const lovelaceBalance = balances.find(b => b.unit === 'lovelace');
+    const tokens = balances.filter(b => b.unit !== 'lovelace');
 
-  return {
-    descriptor: publicKey,
-    balance: lovelaceBalance?.quantity || '0',
-    tokens,
-  };
+    return {
+        descriptor: publicKey,
+        balance: lovelaceBalance?.quantity || '0',
+        tokens,
+    };
 };
--- test/utils/index.test.ts
+++ test/utils/index.test.ts
@@ -3,15 +3,15 @@ import test from 'ava';
 import * as utils from '../../src/utils';
 
 test('prepareMessage', t => {
-  const message = utils.prepareMessage('a', 'b');
+    const message = utils.prepareMessage('a', 'b');
 
-  t.is(message, '{"message":"a","payload":"b"}');
+    t.is(message, '{"message":"a","payload":"b"}');
 });
 
 test('getParams', t => {
-  const params1 = utils.getParams(JSON.stringify('GET_ACCOUNT_INFO'));
+    const params1 = utils.getParams(JSON.stringify('GET_ACCOUNT_INFO'));
 
-  t.is(params1, {
-    command: 'GET_ACCOUNT_INFO',
-  });
+    t.is(params1, {
+        command: 'GET_ACCOUNT_INFO',
+    });
 });
