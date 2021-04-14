4b6c4a2aff7eb6eafd37da9a8c01cbe7cff266bb
1000101 <b1000101@pm.me>
2021-04-14 15:46:01 +0200

    fix(nix): add derivation so that the package could be installed

--- default.nix
+++ default.nix
@@ -1,25 +1,28 @@
 { pkgs ? import <nixpkgs> { } }:
-let
-  src = pkgs.lib.cleanSource ./.;
+rec {
+  blockfrost-websocket-link =
+  let
+    src = pkgs.lib.cleanSource ./.;
 
-  project = pkgs.callPackage ./yarn-project.nix {
-    nodejs = pkgs.nodejs-14_x;
-  } src;
-in project.overrideAttrs (oldAttrs: rec {
+    project = pkgs.callPackage ./yarn-project.nix {
+      nodejs = pkgs.nodejs-14_x;
+    } src;
+  in project.overrideAttrs (oldAttrs: rec {
 
-  name = "blockfrost-websocket-link";
+    name = "blockfrost-websocket-link";
 
-  HOME = "/build";
+    HOME = "/build";
 
-  buildPhase = ''
-    yarn build
+    buildPhase = ''
+      yarn build
 
-    mkdir -p $out/bin
-    cat <<EOF > $out/bin/${name}
-    #!${pkgs.runtimeShell}
-    ${pkgs.nodejs}/bin/node $out/libexec/source/dist/src/server.js
-    EOF
-    chmod +x $out/bin/${name}
-  '';
+      mkdir -p $out/bin
+      cat <<EOF > $out/bin/${name}
+      #!${pkgs.runtimeShell}
+      ${pkgs.nodejs}/bin/node $out/libexec/source/dist/src/server.js
+      EOF
+      chmod +x $out/bin/${name}
+    '';
 
-})
+  });
+}
