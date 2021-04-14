1283cf3a5fb5be4f6c7c53d1f8f920f56a908ba6
1000101 <b1000101@pm.me>
2021-04-14 17:44:41 +0200

    fix(nix): js -> cjs

--- default.nix
+++ default.nix
@@ -19,7 +19,7 @@ rec {
       mkdir -p $out/bin
       cat <<EOF > $out/bin/${name}
       #!${pkgs.runtimeShell}
-      ${pkgs.nodejs}/bin/node --require "$out/libexec/source/.pnp.js" $out/libexec/source/dist/src/server.js
+      ${pkgs.nodejs}/bin/node --require "$out/libexec/source/.pnp.cjs" $out/libexec/source/dist/src/server.js
       EOF
       chmod +x $out/bin/${name}
     '';
