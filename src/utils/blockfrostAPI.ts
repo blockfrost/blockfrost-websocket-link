3458245ae6a4ffd362718f8b0874015e5a642f57
Vladimir Volek <volek.vladimir@gmail.com>
2021-06-20 21:00:30 +0200

    chore: add version to user agent

--- src/utils/blockfrostAPI.ts
+++ src/utils/blockfrostAPI.ts
@@ -1,9 +1,10 @@
 import { BlockFrostAPI } from '@blockfrost/blockfrost-js';
+import packageJson from '../../package.json';
 
 const blockfrostAPI = new BlockFrostAPI({
   projectId: process.env.PROJECT_ID,
   customBackend: process.env.BACKEND_URL || '',
-  userAgent: 'trezor-blockfrost-websocket-link',
+  userAgent: `trezor-blockfrost-websocket-link@${packageJson.version}`,
 });
 
 export { blockfrostAPI };
