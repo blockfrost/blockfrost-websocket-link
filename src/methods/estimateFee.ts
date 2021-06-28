129a204fa4d6c3ba8d68cb617181e92a4b97c013
Vladimir Volek <volek.vladimir@gmail.com>
2021-06-28 12:53:03 +0200

    fix: estimate fee

--- src/methods/estimateFee.ts
+++ src/methods/estimateFee.ts
@@ -3,11 +3,13 @@ import { blockfrostAPI } from '../utils/blockfrostAPI';
 
 export default async (id: number): Promise<string> => {
   try {
+    const result = [];
     const epochsLatest = await blockfrostAPI.epochsLatest();
     const epochsParameters = await blockfrostAPI.epochsParameters(epochsLatest.epoch);
-    const message = prepareMessage(id, {
-      lovelacePerByte: epochsParameters.min_fee_a,
+    result.push({
+      lovelacePerByte: epochsParameters.min_fee_a.toString(),
     });
+    const message = prepareMessage(id, result);
     return message;
   } catch (err) {
     console.log(err);
