3b6a4927c7b06bfc05914d786dfbf77bca4174f8
slowbackspace <slowbackspace@gmail.com>
2021-08-10 18:40:52 +0200

    chore: use generic type in paginate

--- src/utils/common.ts
+++ src/utils/common.ts
@@ -1,13 +1,8 @@
-import { TxIdsToTransactionsResponse } from 'types/transactions';
-
-export const paginate = (
-  transactions: TxIdsToTransactionsResponse[],
-  pageSize: number,
-): TxIdsToTransactionsResponse[][] => {
-  return transactions.reduce((ac, val, i) => {
+export const paginate = <T>(items: T[], pageSize: number): T[][] => {
+  return items.reduce((ac, val, i) => {
     const id = Math.floor(i / pageSize);
     const page = ac[id] || (ac[id] = []);
     page.push(val);
     return ac;
-  }, [] as TxIdsToTransactionsResponse[][]);
+  }, [] as T[][]);
 };
