c85b11c3b74d78ceb4f2f8ea93398dd462c3a78a
Vladimir Volek <volek.vladimir@gmail.com>
2021-10-06 13:20:39 +0200

    fix: tests

--- test/unit/tests/methods/index.ts
+++ test/unit/tests/methods/index.ts
@@ -3,21 +3,12 @@ import estimateFee from '../../../../src/methods/estimateFee';
 describe('methods', () => {
   it('estimateFee', async () => {
     const result = await estimateFee(1);
-    expect(result).toBe({ lovelacePerByte: 44 });
-  });
-
-  it('estimateFee', async () => {
-    const result = await estimateFee(1);
-    expect(result).toBe({ lovelacePerByte: 44 });
-  });
-
-  it('estimateFee', async () => {
-    const result = await estimateFee(1);
-    expect(result).toBe({ lovelacePerByte: 44 });
-  });
-
-  it('estimateFee', async () => {
-    const result = await estimateFee(1);
-    expect(result).toBe({ lovelacePerByte: 44 });
+    expect(result).toBe(
+      JSON.stringify({
+        id: 1,
+        type: 'message',
+        data: { lovelacePerByte: 44 },
+      }),
+    );
   });
 });
