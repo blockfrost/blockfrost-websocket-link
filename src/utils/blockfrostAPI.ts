7aebe0cef7e60783e279099de32877c0e7239133
Vladimir Volek <volek.vladimir@gmail.com>
2021-10-21 16:59:28 +0200

    fix: testnet init sdk

--- src/utils/blockfrostAPI.ts
+++ src/utils/blockfrostAPI.ts
@@ -4,6 +4,7 @@ import packageJson from '../../package.json';
 const blockfrostAPI = new BlockFrostAPI({
   projectId: process.env.PROJECT_ID,
   customBackend: process.env.BACKEND_URL || '',
+  isTestnet: process.env.NETWORK.includes('testnet'),
   userAgent: `${packageJson.name}@${packageJson.version}`,
 });
 
