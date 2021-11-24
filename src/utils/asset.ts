d24ec9fd1eac3b1e3ba36c1ef4f3bfd78d65d118
slowbackspace <slowbackspace@gmail.com>
2021-11-24 13:25:56 +0100

    chore: use accountsAddressesTotal to speed up basic getAccountInfo

--- src/utils/asset.ts
+++ src/utils/asset.ts
@@ -1,4 +1,5 @@
-import { parseAsset } from '@blockfrost/blockfrost-js';
+import { parseAsset, Responses } from '@blockfrost/blockfrost-js';
+import BigNumber from 'bignumber.js';
 import { Balance } from 'types/address';
 import { AssetBalance } from 'types/response';
 
@@ -12,4 +13,14 @@ export const transformAsset = (token: Balance): AssetBalance => {
     fingerprint: parseAsset(token.unit).fingerprint,
     decimals: 0,
   };
+};
+
+export const getAssetBalance = (
+  asset: string,
+  sent: Responses['account_addresses_total']['sent_sum'],
+  received: Responses['account_addresses_total']['received_sum'],
+) => {
+  const receivedAsset = received.find(r => r.unit === asset)?.quantity ?? '0';
+  const sentAsset = sent.find(r => r.unit === asset)?.quantity ?? '0';
+  return new BigNumber(receivedAsset).minus(sentAsset);
 };
