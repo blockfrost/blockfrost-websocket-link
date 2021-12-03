a84a11888eeb8a50e7e56934bd64d453b2c1096d
slowbackspace <slowbackspace@gmail.com>
2021-12-03 19:51:54 +0100

    fix: retrieve metrics in the bg instead of on each request

--- src/utils/prometheus.ts
+++ src/utils/prometheus.ts
@@ -1,3 +1,5 @@
+import { WebSocketServer } from 'ws';
+
 export const jsonToPrometheus = (metrics: Record<string, unknown>): string => {
   let output = '';
   Object.entries(metrics).forEach(([key, value]) => {
@@ -5,3 +7,43 @@ export const jsonToPrometheus = (metrics: Record<string, unknown>): string => {
   });
   return output;
 };
+
+export class MetricsCollector {
+  metrics = {};
+  intervalId: NodeJS.Timer | undefined;
+  wss: WebSocketServer;
+
+  constructor(wss: WebSocketServer, interval: number) {
+    this.wss = wss;
+    this.startCollector(interval);
+  }
+
+  private _collect = () => {
+    return {
+      websocket_link_clients: this.wss.clients.size,
+      // https://nodejs.org/api/process.html#processmemoryusage
+      websocket_link_rss: process.memoryUsage().rss,
+      websocket_link_heap_total: process.memoryUsage().heapTotal,
+      websocket_link_heap_used: process.memoryUsage().heapUsed,
+      websocket_link_external: process.memoryUsage().external,
+      websocket_link_array_buffers: process.memoryUsage().arrayBuffers,
+    };
+  };
+
+  startCollector = (interval: number) => {
+    this.metrics = this._collect();
+    this.intervalId = setInterval(() => {
+      this.metrics = this._collect();
+    }, interval);
+  };
+
+  stopCollector = () => {
+    if (this.intervalId !== undefined) {
+      clearInterval(this.intervalId);
+    }
+  };
+
+  toJson = (): string => {
+    return jsonToPrometheus(this.metrics);
+  };
+}
