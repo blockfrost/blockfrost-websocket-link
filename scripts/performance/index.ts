14444c8dbb5649abad6bad1d51c0a71f92156021
slowbackspace <slowbackspace@gmail.com>
2022-01-07 19:35:46 +0100

    chore: add GET_BALANCE_HISTORY call to test script

--- scripts/performance/index.ts
+++ scripts/performance/index.ts
@@ -22,7 +22,7 @@ class TestSuite {
   addUser = async (xpubs: string[]) => {
     console.log(`Connecting user with ${xpubs.length} accounts... `);
     //  each user has its own websocket client
-    const c = new TestClient(this.wsServer);
+    const c = new TestClient(this.wsServer, false);
     await c.waitForConnection();
 
     // retrieve basic info and subscribe to block notifications
@@ -43,7 +43,7 @@ class TestSuite {
         descriptor: xpub,
       });
       // after each added account call resubscribe with updated addresses
-      c.send('SUBSCRIBE_ADDRESS', {
+      await c.sendAndWait('SUBSCRIBE_ADDRESS', {
         addresses: addresses,
       });
 
@@ -55,8 +55,24 @@ class TestSuite {
           details: 'basic',
         });
       }, 20000);
+
+      // refetch balance history (could happen after receiving/sending tx)
+      setInterval(() => {
+        c.send('GET_BALANCE_HISTORY', {
+          descriptor: xpub,
+          groupBy: 86400, // 1 day
+        });
+      }, randomIntFromInterval(600, 1200) * 1000); // every 10-20 mins
       this.metrics.accounts += 1;
     }
+    for (const xpub of xpubs) {
+      // fetch history for all xpubs of the user
+      c.send('GET_BALANCE_HISTORY', {
+        descriptor: xpub,
+        groupBy: 86400, // 1 day
+      });
+    }
+
     this.metrics.users += 1;
     console.log(
       `Current total users: ${this.metrics.users}, total accounts: ${this.metrics.accounts}`,
@@ -66,7 +82,13 @@ class TestSuite {
   addRandomUserInInterval = (interval: number) => {
     setInterval(() => {
       console.log('Adding new user');
-      this.addUser(generateRandomAccounts(randomIntFromInterval(2, 4)));
+      // 50% change it will be "all seed" user otherwise there will be only empty accounts
+      const r = randomIntFromInterval(0, 10);
+      if (r > 5) {
+        this.addUser(allSeed);
+      } else {
+        this.addUser(generateRandomAccounts(randomIntFromInterval(2, 4)));
+      }
     }, interval);
   };
 }
