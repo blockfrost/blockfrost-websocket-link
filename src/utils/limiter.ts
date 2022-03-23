5e2d0a517326c71822362240c6c625df216a447d
slowbackspace <slowbackspace@gmail.com>
2022-03-23 21:45:25 +0100

    chore: log limiter queue errors

--- src/utils/limiter.ts
+++ src/utils/limiter.ts
@@ -1,4 +1,10 @@
 import PQueue from 'p-queue';
 import { REQUEST_CONCURRENCY } from '../constants/config';
+import { logger } from './logger';
 
 export const pLimiter = new PQueue({ concurrency: REQUEST_CONCURRENCY });
+
+// @ts-expect-error
+pLimiter.on('error', error => {
+  logger.error(error);
+});
