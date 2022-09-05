3aad84aba313dc3768c649ba0b9d99a349ae11a2
slowbackspace <slowbackspace@gmail.com>
2022-09-05 16:50:29 +0200

    chore: timeout for vanilla submit api (aka mempool full handling)

--- src/methods/push-transaction.ts
+++ src/methods/push-transaction.ts
@@ -1,17 +1,30 @@
 import { prepareErrorMessage, prepareMessage } from '../utils/message';
-import { blockfrostAPI } from '../utils/blockfrost-api';
+import { getBlockfrostClient } from '../utils/blockfrost-api';
 import { logger } from '../utils/logger';
+import { BlockfrostClientError } from '@blockfrost/blockfrost-js';
+
+// Special client for tx submit due timeout that's necessary for handling "mempool full" error.
+// Cardano Tx Submit API will just wait indefinitely, so we need to close the connection and return proper error message
+const txClient = getBlockfrostClient({ requestTimeout: 5000 });
 
 export default async (id: number, transaction: Uint8Array | string): Promise<string> => {
   try {
-    const submitTransactionResult = await blockfrostAPI.txSubmit(transaction);
+    const submitTransactionResult = await txClient.txSubmit(transaction);
     const message = prepareMessage(id, submitTransactionResult);
 
     return message;
   } catch (error) {
     logger.error(error);
-    const message = prepareErrorMessage(id, error);
+    if (error instanceof BlockfrostClientError && error.code === 'ETIMEDOUT') {
+      // Request timed out. Most likely mempool is full since that's the only reason why submit api should get stuck
+      const errorMessage = 'Mempool is full, please try resubmitting again later.';
+      const message = prepareErrorMessage(id, errorMessage);
 
-    return message;
+      return message;
+    } else {
+      const message = prepareErrorMessage(id, error);
+
+      return message;
+    }
   }
 };
--- src/utils/blockfrost-api.ts
+++ src/utils/blockfrost-api.ts
@@ -1,12 +1,18 @@
 import { BlockFrostAPI } from '@blockfrost/blockfrost-js';
+import { Options } from '@blockfrost/blockfrost-js/lib/types/index';
 import packageJson from '../../package.json';
 
-const blockfrostAPI = new BlockFrostAPI({
-  projectId: process.env.BLOCKFROST_PROJECT_ID,
-  customBackend: process.env.BLOCKFROST_BACKEND_URL || '',
-  isTestnet: process.env.BLOCKFROST_NETWORK === 'testnet',
-  userAgent: `${packageJson.name}@${packageJson.version}`,
-  rateLimiter: false,
-});
+export const getBlockfrostClient = (options?: Partial<Options>) => {
+  return new BlockFrostAPI({
+    projectId: process.env.BLOCKFROST_PROJECT_ID,
+    customBackend: process.env.BLOCKFROST_BACKEND_URL || '',
+    isTestnet: process.env.BLOCKFROST_NETWORK === 'testnet',
+    userAgent: `${packageJson.name}@${packageJson.version}`,
+    rateLimiter: false,
+    ...options,
+  });
+};
+
+const blockfrostAPI = getBlockfrostClient();
 
 export { blockfrostAPI };
