d9767840cc9bf49f967996f554ea2b9082711d6c
slowbackspace <slowbackspace@gmail.com>
2024-11-28 16:57:48 +0100

    chore: assert cost_models

--- test/e2e/tests/get-protocol-parameters.e2e.test.ts
+++ test/e2e/tests/get-protocol-parameters.e2e.test.ts
@@ -8,20 +8,9 @@ describe('get-protocol-parameters', () => {
   for (const fixture of fixtures) {
     test(fixture.testName, async () => {
       const ws = getWebSocketClient();
-      const { id, type, data: { cost_models, ...data } } = await ws.sendAndWait('GET_PROTOCOL_PARAMETERS');
+      const msg = await ws.sendAndWait('GET_PROTOCOL_PARAMETERS');
 
-      expect({ id, type, data }).toStrictEqual(fixture.result);
-      expect(typeof cost_models).toEqual('object');
-
-      for(const [version, values] of Object.entries(cost_models)) {
-        expect(typeof version).toEqual('string');
-        expect(typeof values).toEqual('object');
-
-        for(const [description, value] of Object.entries(values as any)) {
-          expect(typeof description).toEqual('string');
-          expect(typeof value).toEqual('number');
-        }
-      }
+      expect(msg).toStrictEqual(fixture.result);
     });
   }
 });
